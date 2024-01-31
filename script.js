7d7096bf03d9572f9f8a45b8c249db5a7b105bc8
vladh <hadyak305246@gmail.com>
2024-01-30 23:45:04 -0700

    Check for duplicates between x-o, x-x and o-o

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Tic-Tac-Toe by Vlad Hadyak
 
-
 // Create a game board
 const gameBoard = (function() {
   const rows = 3;
@@ -15,14 +14,13 @@ const gameBoard = (function() {
     };
   }; 
 
-  const getBoard = () => {
-    return board;
-  };
+  const getBoard = () => board;
 
   return {getBoard};
 })();
 
 
+// Check for duplicates
 const checkCoordinates = (function() {
   let playerXPos = [];
   let playerOPos = [];
@@ -39,28 +37,56 @@ const checkCoordinates = (function() {
   };
 
   const matchCondition = () => {
+    // If x match o and vice versa
     for (let i = 0; i < playerXPos.length; i++) {
-      const xPos = playerXPos[i];
-      console.log(xPos);    // get all possible values entered by x, and check if any rows and cols match with other instances
-
+      const xObj = playerXPos[i];
 
       for (let j = 0; j < playerOPos.length; j++) {
-        const oPos = playerOPos[j];
-        if ((xPos.row === oPos.row && xPos.col === oPos.col)) {
-          console.log("matched");
+        const oObj = playerOPos[j];
+
+        if (xObj.row === oObj.row && xObj.col === oObj.col) {
+          console.log("x match o");
         };
       };
+    };
+
+    // Check for o-o and x-x matches
+    const checkDuplicates = (positions) => {        // an array of objects for playerXPos / playerOPos
+      positions.sort((a, b) => {                    // compare one object with another of the same mark, and sort it
+        if (a.row !== b.row) {
+          return a.row - b.row;
+        } else {
+          return a.col - b.col;
+        };
+      });
+
+      for (let i = 0; i < positions.length - 1; i++) {
+        if (positions[i].row === positions[i + 1].row && 
+            positions[i].col === positions[i + 1].col) {
+          return true;
+        };
+      };
+      return false;
+    };
+
+    // If x match x
+    if (checkDuplicates(playerXPos)) {
+      console.log("x match x");    
+    };
+    // If o match o
+    if (checkDuplicates(playerOPos)) {
+      console.log("o match o");
     };
   };
 
   return {playerX, playerO, matchCondition};
 })();
 
-checkCoordinates.playerX(1, 2, "x");
-checkCoordinates.playerO(0, 2, "o");
-checkCoordinates.playerX(1, 1, "x");
-checkCoordinates.playerX(0, 2, "x");
-checkCoordinates.playerO(1, 0, "o");
-checkCoordinates.matchCondition();
+checkCoordinates.playerX(2, 0, "x");
+checkCoordinates.playerO(2, 2, "o");
+checkCoordinates.playerX(2, 0, "x");
+checkCoordinates.playerX(2, 2, "x");
+checkCoordinates.playerO(2, 1, "o");
+checkCoordinates.matchCondition();    
 
 console.log(gameBoard.getBoard());    // Return updated board
